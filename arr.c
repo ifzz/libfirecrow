b41d03ea39e2ce27e2fc314275b8094888f68f68
Fire Crow <fcrow@proclivitysystems.com>
2010-11-29 21:25:43 -0500

    minor changes to alloc function in arr.c

--- arr.c
+++ arr.c
@@ -6,11 +6,13 @@
 /* arr = arr_alloc(); */
 struct arr *arr_alloc(int amount){
 	struct arr *a = (struct arr *) alloc_or_die(sizeof(struct arr), "arr_alloc allocating struct", 1);
+	a->c = 0;
 	if(amount > 0){
 		char *v = (char *) alloc_or_die((size_t) amount, "arr_alloc allocating value", 1);
 		a->v = v;
-		a->c = 0;
 		a->a = amount;
+	}else{
+		a->a = 0;
 	}
 	return a;
 }
