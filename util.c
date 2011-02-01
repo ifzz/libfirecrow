687ba76fba878d2791bb5ccb9e5162456550842f
Fire Crow <fcrow@proclivitysystems.com>
2011-02-01 03:17:59 -0500

    drafting fork_wpipes wrapper

--- util.c
+++ util.c
@@ -51,3 +51,37 @@ int write_int(int fd, int i){
   write(fd, buff, r);
 }
 
+extern int fork_wpipes(char **argv, struct subproc *sp){
+  /* open pipes */
+  int pid, fds[3][2];
+  ERR_IF_LZ( pipe( fds[0] ), "error opening stdin pipe" );
+  ERR_IF_LZ( pipe( fds[1] ), "error opening stdout pipe" );
+  ERR_IF_LZ( pipe( fds[2] ), "error opening stderr pipe" );
+  ERR_IF_LZ( (pid = fork()), "error fork unsuccessful" );
+  struct subproc s = *sp;
+  if(pid == 0){
+    ERR_IF_LZ( dup2( fds[0][0], 0 ), "error duplicating stderr" );
+    ERR_IF_LZ( dup2( fds[1][1], 1 ), "error duplicating stdout" );
+    ERR_IF_LZ( dup2( fds[2][1], 2 ), "error duplicating stdin" );
+    close( fds[0][1] );
+    close( fds[1][0] );
+    close( fds[2][0] );
+    s.pid = 0;
+    s.in = fd[0][0];
+    s.out = fd[1][1];
+    s.err = fd[2][1];
+    s.flags = SP_CHILD;
+    return 0;
+  }else{
+    close( fds[0][0] );
+    close( fds[1][1] );
+    close( fds[2][1] );
+    s.pid = pid;
+    s.in = fd[0][1];
+    s.out = fd[1][0];
+    s.err = fd[2][0];
+    s.flags = SP_PARENT;
+    return 0;
+  }
+}
+
